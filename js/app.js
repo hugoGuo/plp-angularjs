3f5d62712c1779ecf00f5dd6e2735b31fd543c86
Andrew Benfield <andymania@gmail.com>
2015-07-22 00:41:16 +0800

    Fixed the apiUrl for hosting the site on gh-pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var plpApp = angular.module('plpApp', ['ui.router']);
 
-var apiUrl = 'http://localhost:1337';
+var apiUrl = 'http://82.34.201.231:1337';
 
 // ===== Controllers =====
 plpApp.controller('MainController', ['$scope', 'userFactory', function($scope, userFactory) {
